789b3a24ede4bb5eeec92218ee3e43cf9cd4e871
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-05-28 10:53:46 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+const morgan = require('morgan');
+const debug = require('debug')('app:server');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Настройки подключения к базе данных PostgreSQL
+const pool = new Pool({
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'db',
+  database: process.env.DB_NAME || 'postgres',
+  password: process.env.DB_PASSWORD || '1548',
+  port: process.env.DB_PORT || 5432,
+});
+
+// Маршрут для получения данных из базы данных
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM product');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    debug('\nError occurred:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query('SELECT * FROM product WHERE id = $1', [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).send('Product not found');
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    debug('\nError occurred:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
