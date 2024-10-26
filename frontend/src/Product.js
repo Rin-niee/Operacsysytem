e3b59795717b139335cb4e5a21f63e11c157fd9b
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-10-26 18:21:50 +1000

    Add files via upload

---
+++ frontend/src/Product.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import ProductCard from './ProductCard';
+import './App.css';
+import './App.js';
+
+const ProductPage = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/${id}`)
+      .then(response => {
+        console.log('Product fetched:', response.data); // Отладка
+        setProduct(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the product data!', error);
+      });
+  }, [id]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="product">
+      <ProductCard product={product} />
+      </div>
+      );
+};
+export default ProductPage ;
